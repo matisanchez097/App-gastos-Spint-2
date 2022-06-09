4ed5f55d9b81aa580a1dfdfa71ecb835e08b17e3
Matias Sanchez <matisanchez097@gmail.com>
2022-06-09 19:54:19 -0300

    Sprint con prueba

--- script.js
+++ script.js
@@ -1,50 +1,67 @@
-let datos =[];
+let nombres = [];
+let montos = [];
 let total = 0;
-
 function calcular(){
+    total = 0;
     apagar = 0;
-    apagar = parseFloat(total/datos.length).toFixed(2);
+    for (let i=0; i<montos.length; i++){
+        total += parseFloat(montos[i]);
+    }
+    apagar = parseFloat(total/montos.length).toFixed(2);
     var totalM = document.getElementById("total");
     totalM.innerHTML =`
     <div class="flex-item">
-    <p>El total gastado fue: <b>$${total}</b></p>
-    <p>Cada uno debe pagar: <b>$${apagar}</b</p>
-    </div> 
+    <p>El total gastado fue: $${total}</p>
+    <p>Cada uno debe pagar: $${apagar}</p>
     `;
 }
-
 function Agregar (nombre, monto){
-    total += parseFloat(monto);
-    datos.push({
-        name: nombre,
-        montos: monto
-    })
+    nombres.push(nombre);
+    montos.push(monto);
     funImprimir();
     calcular();
 }
-
 function borrarUno(){
-    datos.pop()
+    let name = prompt("Ingrese nombre a borrar")
+    let indice = nombres.indexOf(name);
+    nombres.splice(indice, 1);
+    montos.splice(indice, 1);
     funImprimir();
     calcular();
 }
-
 function funImprimir(){
     var nuevo = document.getElementById("datos");
     nuevo.innerHTML ="";
-    for (let i = 0; i < datos.length; i++){
+    for (let i = 0; i < montos.length; i++){
         nuevo.innerHTML +=`
         <div class="flex-item-input">
-        <p><b>$${datos[i].name}</b> Gasto: <b>$${datos[i].montos}</b></p>
+        <p><b>${nombres[i]}</b> Gasto: <b>$${montos[i]}</b></p>
         </div>
         `;
     }
 }
 
-function download() {
-    var a = document.createElement('a');
-    var file = new Blob([JSON.stringify(datos)], { type: 'text/plain' });
-    a.href = URL.createObjectURL(file);
-    a.download = 'Base de datos.json';
-    a.click();
+function descargarBase() {
+    let datos = [];
+    for (let i = 0; i < montos.length; i++){
+        datos.push({"nombre":nombres[i], "monto":montos[i]}) ;
+        console.log(datos)
+    }
+    let json = JSON.stringify(datos);
+    let blob = new Blob([json], { type: "application/json" });
+    let url = URL.createObjectURL(blob);
+    let link = document.createElement("a");
+    link.href = url;
+    link.download = "datos.json";
+    link.click();
+}
+
+function Leer(){
+    fetch("datos.json")
+        .then(response =>response.json())
+        .then(data=>{
+            for (let i = 0; i < data.length;i++){
+                AgregarNuevo(data[i].nombre, data[i].monto);
+            }
+        });
 }
